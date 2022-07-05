11540c6263b5e014cbe6a963f5cc9f48379b3bbe
OmarGhonimat97 <omar.alghonimat@gmail.com>
2022-07-06 02:57:40 +0300

    lab17

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# web-scraper
+# LAB - Class 17
+## Project: web-scraper
+### Author: Omar Al Ghonimat
+
+### Links and Resources
+
+[Wiki_page](https://en.wikipedia.org/wiki/Foreign_relations_of_Libya_under_Muammar_Gaddafi)
+
+
+#### How to initialize/run your application (where applicable)
+```
+if __name__ == "__main__":
+    print(get_citations_needed_report('https://en.wikipedia.org/wiki/Foreign_relations_of_Libya_under_Muammar_Gaddafi'))
+    print(get_citations_needed_count('https://en.wikipedia.org/wiki/Foreign_relations_of_Libya_under_Muammar_Gaddafi'))
+```
---
+++ web_scraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+wp_url = 'https://en.wikipedia.org/wiki/Foreign_relations_of_Libya_under_Muammar_Gaddafi'
+
+
+def get_citations_needed_report(url):
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    all_paragraphs = soup.findAll("p")
+
+    for p in all_paragraphs:
+        if p.find('span'):
+            print(p)
+
+
+def get_citations_needed_count(url: str):
+
+    count = 0
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    all_paragraphs = soup.findAll("p")
+
+    for p in all_paragraphs:
+        if p.find('span'):
+            count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    print(get_citations_needed_report('https://en.wikipedia.org/wiki/Foreign_relations_of_Libya_under_Muammar_Gaddafi'))
+    print(get_citations_needed_count('https://en.wikipedia.org/wiki/Foreign_relations_of_Libya_under_Muammar_Gaddafi'))
